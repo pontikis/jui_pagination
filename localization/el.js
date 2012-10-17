0ec27c8c49432dae6fedde1526a34532345d6a5f
Christos Pontikis <pontikis@gmail.com>
2012-10-17 22:24:10 +0300

    Localization - ref #7

---
+++ localization/el.js
@@ -0,0 +1,7 @@
+/* jui_pagination simple localization - GREEK ------------------------------- */
+var rsc_jui_pag = {
+    go_to_page: 'Μετακίνηση',
+    rows_per_page: 'Σειρές ανά σελίδα',
+    current_page: 'Σελίδα',
+    total_pages: 'Σύνολο'
+};
---
+++ localization/en.js
@@ -0,0 +1,7 @@
+/* jui_pagination simple localization - ENGLISH ----------------------------- */
+var rsc_jui_pag = {
+    go_to_page: 'Go to page',
+    rows_per_page: 'Rows per page',
+    current_page: 'Page',
+    total_pages: 'Total'
+};
