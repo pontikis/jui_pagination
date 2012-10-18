5d20ece11d9af8a95e6dc4c38936b1ea86d0b3fe
Christos Pontikis <pontikis@gmail.com>
2012-10-18 21:17:11 +0300

    Localization - close #7

--- localization/el.js
+++ localization/el.js
@@ -1,7 +1,13 @@
-/* jui_pagination simple localization - GREEK ------------------------------- */
+/**
+ * jui_pagination simple localization - GREEK
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
 var rsc_jui_pag = {
     go_to_page: 'Μετακίνηση',
     rows_per_page: 'Σειρές ανά σελίδα',
-    current_page: 'Σελίδα',
-    total_pages: 'Σύνολο'
+    page_label: 'Σελίδα',
+    total_pages_label: 'από'
 };
--- localization/en.js
+++ localization/en.js
@@ -1,7 +1,13 @@
-/* jui_pagination simple localization - ENGLISH ----------------------------- */
+/**
+ * jui_pagination simple localization - ENGLISH
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
 var rsc_jui_pag = {
     go_to_page: 'Go to page',
     rows_per_page: 'Rows per page',
-    current_page: 'Page',
-    total_pages: 'Total'
+    page_label: 'Page',
+    total_pages_label: 'of'
 };
